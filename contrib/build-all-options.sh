55a2a50aa2fc3d15c6c6f0d64e32f57aae2c088b
Michael Richardson <mcr@credil.org>
2013-11-28 17:13:00 -0500

    make clean should be silent

--- contrib/build-all-options.sh
+++ contrib/build-all-options.sh
@@ -9,7 +9,7 @@ make clean
 make
 
 # echo with minimal PPPoE server options
-(make clean || exit 0)
+(make --silent clean || exit 0)
 make USE_SERIAL= USE_DEMAND= COMP= USE_FULL_AUTH= USE_ECP= USE_FULL_IPCP= USE_FULL_LCP= USE_EAP= USE_ECP= USE_PAM= PLUGIN=y HAVE_INET6=y
 
 # build with each item alone
@@ -19,14 +19,14 @@ do
     echo "TRYING WITH $f=y"
     echo
     # try with
-    (make clean || exit 0)
+    (make --silent clean || exit 0)
     make $f=y
 
     echo
     echo "TRYING WITH $f=n"
     echo
     # try without
-    (make clean || exit 0)
+    (make --silent clean || exit 0)
     make $f=
 done
 
